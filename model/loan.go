d6193e51578dcab3742c3bb870492412addd859b
1991-bishnu <bishnu.nandi123@gmail.com>
2025-02-20 20:53:17 +0530

    Enhanced loan model as per lastest data

--- model/loan.go
+++ model/loan.go
@@ -15,11 +15,27 @@ type RetrieveLoanReq struct {
 }
 
 type RetrieveLoanRes struct {
-	LoanID          string  `json:"loan_id"`
-	Status          string  `json:"status"`
-	PrincipalAmount int64   `json:"principal_amount"`
-	TotalInterest   int64   `json:"total_interest"`
-	ROI             float64 `json:"roi"`
-	DisbursedAt     string  `json:"disbursed_at"`
-	ApprovedAt      string  `json:"approved_at"`
+	LoanID          string     `json:"loan_id"`
+	Status          string     `json:"status"`
+	PrincipalAmount int64      `json:"principal_amount"`
+	TotalInterest   int64      `json:"total_interest"`
+	ROI             float64    `json:"roi"`
+	DisbursedAt     string     `json:"disbursed_at"`
+	ApprovedAt      string     `json:"approved_at"`
+	CreatedAt       string     `json:"created_at"`
+	Documents       []Document `json:"documents"`
+}
+
+type Document struct {
+	DocumentID string `json:"document_id"`
+	Type       string `json:"type"`
+	URL        string `json:"url"`
+	EmployeeID string `json:"employee_id"`
+	CreatedAt  string `json:"created_at"`
+}
+
+type ApproveLoanReq struct {
+	LoanID     string
+	EmployeeID string `json:"employee_id"`
+	ImageURL   string `json:"image_url"`
 }
